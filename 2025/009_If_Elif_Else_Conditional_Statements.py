963da863256fdffb6743793b7aba1ab178d1b148
Rahiman K <73978407+Rahiman-K@users.noreply.github.com>
2025-09-30 01:58:42 -0400

    Add files via upload

---
+++ 2025/009_If_Elif_Else_Conditional_Statements.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age : "))
+
+18
+if age > 18:
+    print(f"your age is {age} you can vote")
+elif age == 18:
+    print(f"your age is {age} Think wise and vote")
+else:
+    print(f"your age is {age} you cannot vote")
