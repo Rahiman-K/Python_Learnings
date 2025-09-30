a40baecd7320132ad32ff76ec5cbd87f7e12d05a
Rahiman K <73978407+Rahiman-K@users.noreply.github.com>
2025-09-30 11:40:34 +0530

    Create 010_Match_case_statements.py

---
+++ 2025/010_Match_case_statements.py
@@ -0,0 +1,23 @@
+a = int(input("Enter a numbe8r : "))
+
+match a:
+    case 1:
+        print("00000001")
+    case 2:
+        print("00000010")
+    case 3:
+        print("00000011")
+    case 4:
+        print("00000100") 
+    case 5:
+        print("00000101")   
+    case 6:
+        print("00000110")
+    case 7: 
+        print("00000111")
+    case 8:
+        print("00001000")
+    case 9:
+        print("00001001")
+    case 10:
+        print("00001010")       
